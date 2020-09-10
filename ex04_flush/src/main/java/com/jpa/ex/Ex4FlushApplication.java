008662c546cfc2020ffbdf516dc64eb5fb0bf340
PARKJONGHEE\pjh2688 <pjh2688@naver.com>
2020-09-10 15:33:37 +0900

    ex04_flush

---
+++ ex04_flush/src/main/java/com/jpa/ex/Ex4FlushApplication.java
@@ -0,0 +1,13 @@
+package com.jpa.ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex4FlushApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex4FlushApplication.class, args);
+	}
+
+}
